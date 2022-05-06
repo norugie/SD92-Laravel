a88713efa720519cff37e686c8b1ae9ec613cb50
Rugie Ann Barrameda <norugie015@gmail.com>
2022-05-06 15:50:41 -0700

    upload thumbnail for post

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,15 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function fileUpload (Request $request)
-    {
-        // 
-    }
 }
---
+++ app/Http/Controllers/FileUploadController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File; 
+
+class FileUploadController extends Controller
+{
+    public function uploadImage (Object $file, String $type)
+    {
+        // Set path for image
+        if ($type === 'editor' ? $url = '/images/posts' : $url = '/images/thumbnails');
+        $path = url($url) . '/' . $file->getClientOriginalName();
+        $file_name_to_store = $path;
+
+        // Upload image to designated image folder
+        $file->move(public_path($url), $file->getClientOriginalName());
+
+        if ($type === 'editor' ? $file_location = json_encode([ 'location' => $file_name_to_store ]) : $file_location = $file->getClientOriginalName());
+
+        return $file_location;
+    }
+
+    public function deleteImage (File $file)
+    {
+        // $filename = $request->filename;
+        // $path = public_path() . '/images/posts/' . $filename;
+        // if(File::exists( $path )) File::delete($path);
+    }
+}
