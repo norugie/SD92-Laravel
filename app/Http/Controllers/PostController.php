9c532b55d115301a4c11f9196e3d32de6dbec8d3
Rugie Ann Barrameda <norugie015@gmail.com>
2022-05-16 08:58:23 -0700

    save new post created

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,7 @@ use App\Models\Link;
 use App\Models\Category;
 use App\Http\Controllers\FileUploadController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File; 
 
 class PostController extends Controller
 {
@@ -43,10 +44,11 @@ class PostController extends Controller
             ]
         );
 
+        // Initialize Post object
         $post = new Post;
 
         // Post content
-        $post->post_slug = "PST" . rand(1111111111,9999999999);
+        $post->post_slug = "PST" . date('YmdHis') . "-" . rand(11111111111111,99999999999999);
         $post->post_title = $request->post_title;
         $post->post_type = $request->post_opt_type;
         $post->post_desc = $request->post_desc;
@@ -57,22 +59,19 @@ class PostController extends Controller
         $post->post_gcc = $request->post_gcc_autopost;
         $post->post_nlc = $request->post_nlc_autopost;
 
-        // Post author
+        // Post author and assigned school site
         $post->user_id = session('userID');
         $post->department_id = session('schoolToPost');
 
         // Post thumbnail
-        $post_thumbnail = $this->file->uploadImage($request->file('post_thumbnail'), 'thumbnail');
-        echo $post_thumbnail;
-        // $post->save();
-    
-        // // Post categories
-        // $categories = explode(',', $request->post_categories_id);
-        // $post->categories()->attach($categories);
+        $post->post_thumbnail = $this->file->uploadImage($request->file('post_thumbnail'), 'thumbnail');
 
-        // echo $post->id . "<br>";
-        // var_dump($categories);
-        // dd($post);
+        // Post save info
+        $post->save();
+    
+        // Post categories
+        $categories = explode(',', $request->post_categories_id);
+        $post->categories()->attach($categories);
     }
 
     // Categories
